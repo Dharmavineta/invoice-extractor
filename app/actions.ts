c60408dd37b50d0ebfe09c5eef92808f094c79b0
Krishna Bharadwaj <krishnabharadwaj1995@gmail.com>
2024-02-05 13:32:49 +0530

    update

--- app/actions.ts
+++ app/actions.ts
@@ -23,7 +23,7 @@ export const handleImage = async ({
 
   const parser = new StringOutputParser();
   const prompt =
-    "You're a professional invoice information extractor. You must read the image given to you and output the information contained in the invoice in point by point format. Don't entertain unrelated pictures and decline respectfully reiterating you're purpose as invoice extractor. Be extremely accurate in your analysis and avoid unnecessary jargons. If you can't extract information prompt the user to re-upload the image. Use clear markdown as and when needed, and give the answer in clear markdown. Avoid confusion and be detailed in your analysis.";
+    "You're a professional invoice information extractor. You must read the image given to you and extract the information contained in the invoice in point by point format. Don't entertain unrelated pictures and decline respectfully reiterating you're purpose as invoice extractor. Be extremely accurate in your analysis and avoid unnecessary jargons. If you can't extract information prompt the user to re-upload the image. Use clear markdown as and when needed, and give the answer in clear markdown. Avoid confusion and be detailed in your analysis. Maintain a consistent format";
 
   const input2 = [
     new HumanMessage({
